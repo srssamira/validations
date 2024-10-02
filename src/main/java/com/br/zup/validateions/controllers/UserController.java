d46a6e1708d7a61982ff9243c3ff25d8c23957bc
Samira <srs.samira.silva@gmail.com>
2024-10-02 11:52:25 -0300

    add validations

--- src/main/java/com/br/zup/validateions/controllers/UserController.java
+++ src/main/java/com/br/zup/validateions/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.br.zup.validateions.controllers;
 import com.br.zup.validateions.controllers.dtos.UserDTO;
 import com.br.zup.validateions.controllers.dtos.UserRegisterDTO;
 import com.br.zup.validateions.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,7 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UserDTO registerUser(@RequestBody UserRegisterDTO user){
+    public UserDTO registerUser(@RequestBody @Valid UserRegisterDTO user){
         return userService.save(user);
     }
 }
--- src/main/java/com/br/zup/validateions/controllers/dtos/UserRegisterDTO.java
+++ src/main/java/com/br/zup/validateions/controllers/dtos/UserRegisterDTO.java
@@ -1,9 +1,28 @@
 package com.br.zup.validateions.controllers.dtos;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+
 public class UserRegisterDTO {
+    @NotNull(message = "campo obrigatório")
+    @NotBlank(message = "campo não pode estar em branco")
+    @Pattern(regexp = "^[A-Za-zÀ-ÖØ-öø-ÿ]+(?:\\s[A-Za-zÀ-ÖØ-öø-ÿ]+)+$", message = "deve contar nome e sobrenome, sem numeros ou caracteres especiais")
     private String name;
+
+    @NotNull(message = "campo obrigatório")
+    @Min(value = 18)
+    @Max(value = 100)
+    @Positive(message = "idade tem que ser maior do que 0")
     private int age;
+
+    @NotNull(message = "campo obrigatório")
+    @Min(value = 2024 - 110)
+    @Max(value = 2024)
     private int yearOfBirth;
+
+    @NotNull(message = "campo obrigatório")
+    @NotBlank(message = "campo não pode estar em branco")
+    @Pattern(regexp = "^[a-zA-Z0-9._]+@[a-zA-Z]+\\.[a-z-A-Z]{2,}$", message = "deve estar no formato exemplo@algumacoisa.outracoisa, esteja à vontade para usar letras, números, pontos e-ou underlines antes do @")
     private String email;
 
     public UserRegisterDTO() {
